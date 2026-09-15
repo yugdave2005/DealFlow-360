9a09349def077a83bfe0861fca0b993af46c4bdb
Samarth <samarththakkar841@gmail.com>
2026-09-15 17:34:43 +0530

    remove obsolete test and seed scripts and update schema and package configuration

--- Backend/test-check.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function test() {
-  const quotation = await prisma.quotation.findFirst({
-    where: { id: "359731f3-5b92-426c-8219-6b84dca1f12e" }
-  });
-  console.log("Quotation:", quotation);
-}
-test();
--- Backend/test-status.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function run() {
-  const qs = await prisma.quotation.findMany({ select: { quotationNumber: true, status: true }});
-  console.log(qs);
-}
-run();
